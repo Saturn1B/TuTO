bef3619554f417292b68fc03c18969756aff1f63
julien machado <julienmachado230@gmail.com>
2020-03-24 17:59:45 +0100

    added weapon script

---
+++ juli1 tu pue la merde/juli1 tu pue la merde/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juli1_tu_pue_la_merde
+{
+    class Weapon
+    {
+    }
+}
